289f4a448828323542be151beb148ef55a9ecc5f
unknown <franciscolfraga@gmail.com>
2018-12-03 15:53:51 +0000

    minor changes on encoding

--- actions/addNews.php
+++ actions/addNews.php
@@ -1,5 +1,5 @@
 <?php
-header ('Content-type: text/html; charset=utf-8');
+header ('Content-type: text/html; charset=cp1252');
 function load_news($start){
   $xml=simplexml_load_file("https://sigarra.up.pt/feup/pt/noticias_web.atom?p_grupo_noticias=47");
   if($xml){
@@ -10,16 +10,14 @@ function load_news($start){
         <div class="news-container">
       <?php
                 $url = $news->link['href'];
-                $content = file_get_contents_utf8($url);
+                $content = file_get_contents($url);
                 $first_step = explode( '<div id="conteudoinner">' , $content );
                 $second_step = explode("</div>" , $first_step[1] );
                 $string = str_replace('<img src="', '<img src="https://sigarra.up.pt/feup/pt/', $second_step[0]);
-                #$string = preg_replace_callback("/src=[\'\"](.*?)[\'\"]/", "urlHandler", $string);
+                $string = preg_replace_callback("/src=[\'\"](.*?)[\'\"]/", "urlHandler", $string);
                 echo $string;
 
                 #https://sigarra.up.pt/feup/pt/noticias_geral.noticias_cont?p_id=F1046967137/Convite%20Lan%C3%A7amento%20do%20livro%20Eletr%C3%B3nica.jpg
-
-                #https://sigarra.up.pt/feup/pt/noticias_geral.noticias_cont?p_id=F1046967137/Convite%20Lan%EF%BF%BDamento%20do%20livro%20Eletr%EF%BF%BDnica.jpg
 
                 #https://sigarra.up.pt/feup/pt/noticias_geral.noticias_cont?p_id=F1046967137/Convite%20Lan%E7amento%20do%20livro%20Eletr%F3nica.jpg
       ?>
@@ -35,14 +33,12 @@ function load_news($start){
   }
 }
 
-function file_get_contents_utf8($fn) {
-  $string = file_get_contents($fn);
-  return utf8_encode($string);
-  }
 
   function urlHandler($matches) {
-    return "src='" . utf8_decode($matches[1]) . "'";
+    $srcUrl = $matches[1];
+    return "src='" .  iconv("ISO-8859-1", "Windows-1252//TRANSLIT", $srcUrl) . "'";
   }
+
 
 
 if (isset($_POST['start'])) {
